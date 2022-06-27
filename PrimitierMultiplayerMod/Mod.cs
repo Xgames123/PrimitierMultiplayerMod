74fe9c293232ee5b6aedb1d196732d91a6016dbc
Xgames <Sibe@SIBE-GAMING>
2022-06-27 14:08:58 +0200

    Add project files.

---
+++ PrimitierMultiplayerMod/Mod.cs
@@ -0,0 +1,48 @@
+﻿using Il2CppSystem;
+using PrimitierModdingFramework;
+using PrimitierModdingFramework.Debugging;
+using UnityEngine;
+using UnhollowerBaseLib;
+using UnhollowerRuntimeLib;
+
+namespace PrimitierMultiplayerMod
+{
+
+	public class Mod : PrimitierMod
+    {
+
+
+
+		public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+		{
+			base.OnSceneWasLoaded(buildIndex, sceneName);
+
+			PMFLog.Message("Hello Primitier!");
+
+			
+		}
+		public override void OnRealyLateStart()
+		{
+			base.OnRealyLateStart();
+			
+		}
+
+		public override void OnApplicationStart()
+		{
+			base.OnApplicationStart();
+			PMFSystem.EnableSystem<PMFHelper>();
+		}
+		
+		public override void OnUpdate()
+		{
+			base.OnUpdate();
+		}
+
+		public override void OnFixedUpdate()
+		{
+			base.OnFixedUpdate();
+		}
+
+
+	}
+}
