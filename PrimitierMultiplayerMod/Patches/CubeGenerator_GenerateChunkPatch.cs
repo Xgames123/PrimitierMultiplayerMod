aea1f8a6e091be272c75f8ed4e262f230bf292d0
Xgames <Sibe@SIBE-GAMING>
2022-07-09 12:05:21 +0200

    Fixed world gen

--- PrimitierMultiplayerMod/Patches/CubeGenerator_GenerateChunkPatch.cs
+++ PrimitierMultiplayerMod/Patches/CubeGenerator_GenerateChunkPatch.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿#if false
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,3 +18,4 @@ namespace PrimitierMultiplayerMod.Patches
 
 	}
 }
+#endif
--- PrimitierMultiplayerMod/Patches/TerrainGenerator_GeneratePatch.cs
+++ PrimitierMultiplayerMod/Patches/TerrainGenerator_GeneratePatch.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿#if false
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,3 +19,4 @@ namespace PrimitierMultiplayerMod.Patches
 
 	}
 }
+#endif
